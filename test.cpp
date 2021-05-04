55387aca94242ee25df840da191ef4d52f313f0a
Lê Văn Linh <69961463+levanlinhSR1812@users.noreply.github.com>
2021-05-04 20:30:09 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,5 +3,14 @@
 
 int main(int argc, char* argv){
   cv::Mat img;
-  img = cv::imread(
+  img = cv::imread('lena.jpg', cv::IMREAD_COLOR);
+  
+  if(!img.data){
+    std::cout << "Image not found" << std::endl;
+    return -1;
+  }
+  
+  cv::imshow("Lena", img);
+  cv::waitKey(0);
+  cv::destroyAllWindows();
 }
